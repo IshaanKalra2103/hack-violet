71db53bf8c7a20f004d35d0794deea6b3eceff00
Parsh22 <70302713+Parsh22@users.noreply.github.com>
2024-02-04 10:21:10 -0500

    Update EmailController.java

--- backend-java./EmailController.java
+++ backend-java./EmailController.java
@@ -1,1 +1,20 @@
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("goelparsh@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
