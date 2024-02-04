685003f9c87cc419917405df47c0114cbb5ac2fc
Parsh22 <70302713+Parsh22@users.noreply.github.com>
2024-02-04 10:12:56 -0500

    Update Main.java

--- backend-java./Main.java
+++ backend-java./Main.java
@@ -1,1 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class GoMamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoMamaApplication.class, args);
+    }
+
+}
